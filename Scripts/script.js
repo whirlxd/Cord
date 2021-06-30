42e65cec752ad765997af66f4657d24dd81f362a
Whirl <71062861+Whirl21@users.noreply.github.com>
2021-06-30 19:55:47 +0530

    Delete Scripts directory

--- Scripts/script.js
+++
@@ -1,9 +0,0 @@
-function showPreview(){
-    var htmlCode = document.getElementById("html").value;
-    var cssCode = "<style>"+document.getElementById("css").value+"</style>";
-    var jsCode = "<scri"+"pt>"+document.getElementById("js").value+"</scri"+"pt>";
-    var frame = document.getElementById("preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
-  }
